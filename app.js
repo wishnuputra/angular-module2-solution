8f336c407ec58e68492e1a2c0086d00678caeb25
wishnuputra <wishnuputra.dhanu@gmail.com>
2019-07-27 12:49:05 +0700

    add notification message

--- app.js
+++ app.js
@@ -6,26 +6,56 @@ angular.module('ShoppingListCheckOff', [])
 .controller('AlreadyBoughtController', AlreadyBoughtController)
 .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-/* Function: ToBuyController
- *
- */
+/* Function: ToBuyController */
 ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyController(ShoppingListCheckOffService) {
+  var buyList = this;
 
+  buyList.items = ShoppingListCheckOffService.getToBuyList();
+
+  buyList.removeItem = function (itemIndex) {
+      ShoppingListCheckOffService.removeItem(itemIndex)
+  };
 }
 
-/* Function: AlreadyBoughtController
- *
- */
+/* Function: AlreadyBoughtController */
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtList = this;
 
+  boughtList.items = ShoppingListCheckOffService.getAlreadyBoughtList();
 }
 
+/* Service: ShoppingListCheckOff */
+function ShoppingListCheckOffService() {
+  var service = this;
 
+  // List of to buy items
+  var toBuyList = [
+    {name: "cookies", quantity: 10},
+    {name: "soda drinks", quantity: 20},
+    {name: "choco bar", quantity: 5},
+    {name: "pop corn", quantity: 15},
+    {name: "ice cream", quantity: 25}
+  ];
 
+  // List of already bought items
+  var alreadyBoughtList = [];
 
+  service.removeItem = function (itemIndex) {
+    var item = toBuyList[itemIndex];
 
+    alreadyBoughtList.push(item);
+    toBuyList.splice(itemIndex, 1);
+  };
 
+  service.getToBuyList = function () {
+    return toBuyList;
+  };
+
+  service.getAlreadyBoughtList = function () {
+    return alreadyBoughtList;
+  };
+}
 
 })();
